4a3f0d711dcdfa1c0f227aa1f7a8e0bd4621c0d1
osedhelu <osedhelu@gmial.com>
2020-10-05 10:10:17 -0500

    nueva actualizacion

--- assets/js/personal.js
+++ assets/js/personal.js
@@ -1,9 +1,29 @@
+function readerMenu() {
+  console.log("ver")
+  setTimeout(() => {
+      // Add minus icon for collapse element which is open by default
+    $(".collapse.show").each(function(){
+        $(this).prev(".card-header-my").find(".fa").addClass("fa-minus").removeClass("fa-plus");
+    });
+    
+    // Toggle plus minus icon on show hide of collapse element
+    $(".collapse").on('show.bs.collapse', function(){
+      // console.log(this)
 
-
+        $(this).prev(".card-header-my").find(".fa").removeClass("fa-plus").addClass("fa-minus");
+    }).on('hide.bs.collapse', function(){
+        $(this).prev(".card-header-my").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+    });
+  }, 2500)
+  // console.log("_absdk")
+   
+}
 function xzoom() {
+    initXzoom();
+
        (function ($) {
        $(document).ready(function() {
-           $('.xzoom2, .xzoom-gallery2').xzoom({position: '#xzoom2-id', tint: '#6c757d'});
+           $('.xzoom2').xzoom({position: '#xzoom2-id', tint: '#6c757d'});
    
            //Integration with hammer.js
            var isTouchSupported = 'ontouchstart' in window;
